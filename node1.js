9ddbcd3aeb2f5ab355409ee985e5347b098b505b
ALRASASA <88656552+ALRASASA@users.noreply.github.com>
2023-05-25 02:45:05 -0700

    Add files via upload

---
+++ node1.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer((req,res)=>{
+
+    res.writeHead(200, {'content-type': 'text/plain'})
+            res.end('HELLO WORLD')
+            console.log('hello world started')
+}).listen(5000);
+
+console.log('Hello world');
