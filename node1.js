d1f6b4579e8c5e0076b62d4ef573c19da4531b0d
ALRASASA <88656552+ALRASASA@users.noreply.github.com>
2023-05-25 02:44:34 -0700

    Delete node1.js

--- node1.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer((req,res)=>{
-
-    res.writeHead(200, {'content-type': 'text/plain'})
-            res.end('HELLO WORLD')
-            console.log('hello world started')
-}).listen(5000);
